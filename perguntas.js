bfdc38e38dc06a78d2c05177f33e537a9a2f9af7
kamily0schmitt <schmitt.kamily.f@gmail.com>
2024-10-17 22:40:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é fotossíntese?',
+    'Fotossíntese é o processo em que as plantas convertem luz solar em energia.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "bom dia" em inglês?',
+    'Bom dia" em inglês é "Good morning" (GUD MORNIN).'
+)
